d9b7e35516db3c781b6c363a16149853d6871e8a
Lucas Rabelo <50274278+marreapato@users.noreply.github.com>
2024-04-24 19:37:31 -0300

    Create passageiros.sql

---
+++ projeto_final/inserts/passageiros.sql
@@ -0,0 +1,202 @@
+INSERT INTO PASSAGEIRO_TB VALUES (
+    '12345678901',
+    'Carlos Silva',
+    'M',
+    TO_DATE('15/07/1980', 'DD/MM/YYYY'),
+    tp_endereco('Brasil', '12345678', 'SP', 'São Paulo', 'Rua A, 123'),
+    tp_fones(TP_FONE('55', '11', '987654321')),
+    'carlos.silva@email.com'
+);
+
+
+INSERT INTO PASSAGEIRO_TB VALUES (
+    '98765432198',
+    'Ana Souza',
+    'F',
+    TO_DATE('25/05/1995', 'DD/MM/YYYY'),
+    tp_endereco('Brasil', '87654321', 'RJ', 'Rio de Janeiro', 'Av. B, 456'),
+    tp_fones(TP_FONE('55', '21', '999876543')),
+    'ana.souza@email.com'
+);
+
+INSERT INTO PASSAGEIRO_TB VALUES (
+    '11122233344',
+    'Lucas Santos',
+    'M',
+    TO_DATE('10/10/1988', 'DD/MM/YYYY'),
+    tp_endereco('Brasil', '54321098', 'MG', 'Belo Horizonte', 'Rua C, 789'),
+    tp_fones(TP_FONE('55', '31', '987654321')),
+    'lucas.santos@email.com'
+);
+
+INSERT INTO PASSAGEIRO_TB VALUES (
+    '55566677788',
+    'Juliana Oliveira',
+    'F',
+    TO_DATE('03/12/1992', 'DD/MM/YYYY'),
+    tp_endereco('Brasil', '65432109', 'RS', 'Porto Alegre', 'Rua D, 321'),
+    tp_fones(TP_FONE('55', '51', '999887766')),
+    'juliana.oliveira@email.com'
+);
+
+INSERT INTO PASSAGEIRO_TB VALUES (
+    '99988877766',
+    'Rafael Costa',
+    'M',
+    TO_DATE('20/02/1987', 'DD/MM/YYYY'),
+    tp_endereco('Brasil', '13579024', 'PR', 'Curitiba', 'Av. E, 567'),
+    tp_fones(TP_FONE('55', '41', '998877665')),
+    'rafael.costa@email.com'
+);
+
+INSERT INTO PASSAGEIRO_TB VALUES (
+    '77788899911',
+    'Maria Santos',
+    'F',
+    TO_DATE('12/09/1990', 'DD/MM/YYYY'),
+    tp_endereco('Brasil', '98765432', 'BA', 'Salvador', 'Rua F, 789'),
+    tp_fones(TP_FONE('55', '71', '999888777')),
+    'maria.santos@email.com'
+);
+
+INSERT INTO PASSAGEIRO_TB VALUES (
+    '44455566677',
+    'Thiago Oliveira',
+    'M',
+    TO_DATE('28/04/1985', 'DD/MM/YYYY'),
+    tp_endereco('Brasil', '54321098', 'MG', 'Belo Horizonte', 'Av. G, 456'),
+    tp_fones(TP_FONE('55', '31', '987654321')),
+    'thiago.oliveira@email.com'
+);
+
+INSERT INTO PASSAGEIRO_TB VALUES (
+    '33322211100',
+    'Camila Lima',
+    'F',
+    TO_DATE('05/11/1993', 'DD/MM/YYYY'),
+    tp_endereco('Brasil', '12345678', 'SP', 'São Paulo', 'Rua H, 321'),
+    tp_fones(TP_FONE('55', '11', '999333222')),
+    'camila.lima@email.com'
+);
+
+INSERT INTO PASSAGEIRO_TB VALUES (
+    '88877766655',
+    'Gustavo Silva',
+    'M',
+    TO_DATE('20/07/1982', 'DD/MM/YYYY'),
+    tp_endereco('Brasil', '87654321', 'RJ', 'Rio de Janeiro', 'Av. I, 234'),
+    tp_fones(TP_FONE('55', '21', '999444555')),
+    'gustavo.silva@email.com'
+);
+
+INSERT INTO PASSAGEIRO_TB VALUES (
+    '22233344455',
+    'Renata Pereira',
+    'F',
+    TO_DATE('15/03/1998', 'DD/MM/YYYY'),
+    tp_endereco('Brasil', '98765432', 'PR', 'Curitiba', 'Rua J, 567'),
+    tp_fones(TP_FONE('55', '41', '999555666')),
+    'renata.pereira@email.com'
+);
+
+INSERT INTO PASSAGEIRO_TB VALUES (
+    '99911122233',
+    'Pedro Oliveira',
+    'M',
+    TO_DATE('18/08/2010', 'DD/MM/YYYY'),
+    tp_endereco('Brasil', '54321098', 'MG', 'Belo Horizonte', 'Av. K, 789'),
+    tp_fones(TP_FONE('55', '31', '987654321')),
+    'pedro.oliveira@email.com'
+);
+
+INSERT INTO PASSAGEIRO_TB VALUES (
+    '77788899900',
+    'Beatriz Santos',
+    'F',
+    TO_DATE('10/05/2009', 'DD/MM/YYYY'),
+    tp_endereco('Brasil', '12345678', 'SP', 'São Paulo', 'Rua L, 123'),
+    tp_fones(TP_FONE('55', '11', '999888777')),
+    'beatriz.santos@email.com'
+);
+
+
+INSERT INTO PASSAGEIRO_TB VALUES (
+    '55544433322',
+    'Felipe Lima',
+    'M',
+    TO_DATE('03/12/2012', 'DD/MM/YYYY'),
+    tp_endereco('Brasil', '87654321', 'RJ', 'Rio de Janeiro', 'Av. M, 456'),
+    tp_fones(TP_FONE('55', '21', '999777888')),
+    'felipe.lima@email.com'
+);
+
+INSERT INTO PASSAGEIRO_TB VALUES (
+    '66677788899',
+    'Isabela Costa',
+    'F',
+    TO_DATE('25/10/2011', 'DD/MM/YYYY'),
+    tp_endereco('Brasil', '98765432', 'PR', 'Curitiba', 'Rua N, 321'),
+    tp_fones(TP_FONE('55', '41', '999666777')),
+    'isabela.costa@email.com'
+);
+
+INSERT INTO PASSAGEIRO_TB VALUES (
+    '33322211144',
+    'Matheus Pereira',
+    'M',
+    TO_DATE('12/03/2008', 'DD/MM/YYYY'),
+    tp_endereco('Brasil', '65432109', 'RS', 'Porto Alegre', 'Av. O, 567'),
+    tp_fones(TP_FONE('55', '51', '999333222')),
+    'matheus.pereira@email.com'
+);
+
+INSERT INTO PASSAGEIRO_TB VALUES (
+    '99913122233',
+    'Pedro Oliveira',
+    'M',
+    TO_DATE('18/08/2010', 'DD/MM/YYYY'),
+    tp_endereco('Brasil', '54321098', 'MG', 'Belo Horizonte', 'Av. K, 789'),
+    tp_fones(TP_FONE('55', '31', '987654321')),
+    'pedro.oliveira@email.com'
+);
+
+INSERT INTO PASSAGEIRO_TB VALUES (
+    '77783899900',
+    'Beatriz Santos',
+    'F',
+    TO_DATE('10/05/2009', 'DD/MM/YYYY'),
+    tp_endereco('Brasil', '12345678', 'SP', 'São Paulo', 'Rua L, 123'),
+    tp_fones(TP_FONE('55', '11', '999888777')),
+    'beatriz.santos@email.com'
+);
+
+
+INSERT INTO PASSAGEIRO_TB VALUES (
+    '55543433322',
+    'Felipe Lima',
+    'M',
+    TO_DATE('03/12/2012', 'DD/MM/YYYY'),
+    tp_endereco('Brasil', '87654321', 'RJ', 'Rio de Janeiro', 'Av. M, 456'),
+    tp_fones(TP_FONE('55', '21', '999777888')),
+    'felipe.lima@email.com'
+);
+
+INSERT INTO PASSAGEIRO_TB VALUES (
+    '66677388899',
+    'Isabela Costa',
+    'F',
+    TO_DATE('25/10/2011', 'DD/MM/YYYY'),
+    tp_endereco('Brasil', '98765432', 'PR', 'Curitiba', 'Rua N, 321'),
+    tp_fones(TP_FONE('55', '41', '999666777')),
+    'isabela.costa@email.com'
+);
+
+INSERT INTO PASSAGEIRO_TB VALUES (
+    '33322311144',
+    'Matheus Pereira',
+    'M',
+    TO_DATE('12/03/2008', 'DD/MM/YYYY'),
+    tp_endereco('Brasil', '65432109', 'RS', 'Porto Alegre', 'Av. O, 567'),
+    tp_fones(TP_FONE('55', '51', '999333222')),
+    'matheus.pereira@email.com'
+);
