04660aced7c3fa03b2638d1a4c3b53653e9d527b
Lucas Rabelo <50274278+marreapato@users.noreply.github.com>
2024-04-24 20:34:01 -0300

    Create compras.sql

---
+++ projeto_final/inserts/compras.sql
@@ -0,0 +1,28 @@
+-- Inserting purchase data into COMPRA_TB
+
+-- Inserting a purchase record for a passenger and ticket combination
+INSERT INTO COMPRA_TB VALUES (
+    tp_compra(
+        TO_DATE('15/01/2024', 'DD/MM/YYYY'), -- data_compra
+        (SELECT REF(p) FROM PASSAGEIRO_TB p WHERE p.PK_CPF = '12345678901'), -- passageiros REF
+        (SELECT REF(pt) FROM PASSAGEM pt WHERE pt.pk_localizador_passagem = 1) -- passagem REF
+    )
+);
+
+-- Inserting another purchase record for a different passenger and ticket
+INSERT INTO COMPRA_TB VALUES (
+    tp_compra(
+        TO_DATE('20/01/2024', 'DD/MM/YYYY'), -- data_compra
+        (SELECT REF(p) FROM PASSAGEIRO_TB p WHERE p.PK_CPF = '98765432198'), -- passageiros REF
+        (SELECT REF(pt) FROM PASSAGEM pt WHERE pt.pk_localizador_passagem = 2) -- passagem REF
+    )
+);
+
+-- Inserting another purchase record for a different passenger and ticket
+INSERT INTO COMPRA_TB VALUES (
+    tp_compra(
+        TO_DATE('05/01/2024', 'DD/MM/YYYY'), -- data_compra
+        (SELECT REF(p) FROM PASSAGEIRO_TB p WHERE p.PK_CPF = '11122233344'), -- passageiros REF
+        (SELECT REF(pt) FROM PASSAGEM pt WHERE pt.pk_localizador_passagem = 3) -- passagem REF
+    )
+);
