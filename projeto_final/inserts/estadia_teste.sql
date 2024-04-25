c039c1d0af33773b7b67c59722a7799e43068827
Lucas Rabelo <50274278+marreapato@users.noreply.github.com>
2024-04-24 21:07:44 -0300

    Create estadia_teste.sql

---
+++ projeto_final/inserts/estadia_teste.sql
@@ -0,0 +1,9 @@
+INSERT INTO ESTADIA VALUES (
+    ESTADIA_TP(
+        1, -- pk_cod_estadia
+        1000.00, -- valor_estadia
+        TO_DATE('2024-05-10', 'YYYY-MM-DD'), -- data_check_in
+        TO_DATE('2024-05-20', 'YYYY-MM-DD'), -- data_check_out
+        TP_NT_COMPRA(TP_COMPRA(SELECT REF(pt) FROM compra_tb pt WHERE pt.id_compra = 1))
+    )
+);
