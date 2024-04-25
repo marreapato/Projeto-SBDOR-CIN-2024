8912f82c3f9d3d152b1541072e2056effc494015
Lucas Rabelo <50274278+marreapato@users.noreply.github.com>
2024-04-25 20:10:24 -0300

    Update estadia_teste.sql

--- projeto_final/inserts/estadia_teste.sql
+++ projeto_final/inserts/estadia_teste.sql
@@ -16,112 +16,16 @@ INSERT INTO ESTADIA VALUES (
 
 INSERT INTO ESTADIA VALUES (
     ESTADIA_TP(
-        1, -- pk_cod_estadia
+        2, -- pk_cod_estadia
         1000.00, -- valor_estadia
         TO_DATE('14/01/2025', 'DD/MM/YYYY'),
   		TO_DATE('17/01/2025', 'DD/MM/YYYY'), -- data_check_out
-        TP_NT_RESERVA(TP_RESERVA((SELECT REF(pt) FROM compra_tb pt WHERE pt.id_compra = 20)))
+        TP_NT_RESERVA(TP_RESERVA((SELECT REF(pt) FROM compra_tb pt WHERE pt.id_compra = 2)))
     )
 );
 
 
-INSERT INTO ESTADIA VALUES (
-    ESTADIA_TP(
-        1, -- pk_cod_estadia
-        1000.00, -- valor_estadia
-        TO_DATE('14/01/2025', 'DD/MM/YYYY'),
-  		TO_DATE('17/01/2025', 'DD/MM/YYYY'), -- data_check_out
-        TP_NT_RESERVA(TP_RESERVA((SELECT REF(pt) FROM compra_tb pt WHERE pt.id_compra = 10)))
-    )
-);
 
 
-INSERT INTO ESTADIA VALUES (
-    ESTADIA_TP(
-        1, -- pk_cod_estadia
-        1000.00, -- valor_estadia
-        TO_DATE('2024-05-10', 'YYYY-MM-DD'), -- data_check_in
-        TO_DATE('2024-05-20', 'YYYY-MM-DD'), -- data_check_out
-        TP_NT_RESERVA(TP_RESERVA((SELECT REF(pt) FROM compra_tb pt WHERE pt.id_compra = 1)))
-    )
-);
-
-
-INSERT INTO ESTADIA VALUES (
-    ESTADIA_TP(
-        1, -- pk_cod_estadia
-        1000.00, -- valor_estadia
-        TO_DATE('2024-05-10', 'YYYY-MM-DD'), -- data_check_in
-        TO_DATE('2024-05-20', 'YYYY-MM-DD'), -- data_check_out
-        TP_NT_RESERVA(TP_RESERVA((SELECT REF(pt) FROM compra_tb pt WHERE pt.id_compra = 1)))
-    )
-);
-
-
-INSERT INTO ESTADIA VALUES (
-    ESTADIA_TP(
-        1, -- pk_cod_estadia
-        1000.00, -- valor_estadia
-        TO_DATE('2024-05-10', 'YYYY-MM-DD'), -- data_check_in
-        TO_DATE('2024-05-20', 'YYYY-MM-DD'), -- data_check_out
-        TP_NT_RESERVA(TP_RESERVA((SELECT REF(pt) FROM compra_tb pt WHERE pt.id_compra = 1)))
-    )
-);
-
-
-INSERT INTO ESTADIA VALUES (
-    ESTADIA_TP(
-        1, -- pk_cod_estadia
-        1000.00, -- valor_estadia
-        TO_DATE('2024-05-10', 'YYYY-MM-DD'), -- data_check_in
-        TO_DATE('2024-05-20', 'YYYY-MM-DD'), -- data_check_out
-        TP_NT_RESERVA(TP_RESERVA((SELECT REF(pt) FROM compra_tb pt WHERE pt.id_compra = 1)))
-    )
-);
-
-
-INSERT INTO ESTADIA VALUES (
-    ESTADIA_TP(
-        1, -- pk_cod_estadia
-        1000.00, -- valor_estadia
-        TO_DATE('2024-05-10', 'YYYY-MM-DD'), -- data_check_in
-        TO_DATE('2024-05-20', 'YYYY-MM-DD'), -- data_check_out
-        TP_NT_RESERVA(TP_RESERVA((SELECT REF(pt) FROM compra_tb pt WHERE pt.id_compra = 1)))
-    )
-);
-
-INSERT INTO ESTADIA VALUES (
-    ESTADIA_TP(
-        1, -- pk_cod_estadia
-        1000.00, -- valor_estadia
-        TO_DATE('2024-05-10', 'YYYY-MM-DD'), -- data_check_in
-        TO_DATE('2024-05-20', 'YYYY-MM-DD'), -- data_check_out
-        TP_NT_RESERVA(TP_RESERVA((SELECT REF(pt) FROM compra_tb pt WHERE pt.id_compra = 1)))
-    )
-);
-
-
-INSERT INTO ESTADIA VALUES (
-    ESTADIA_TP(
-        1, -- pk_cod_estadia
-        1000.00, -- valor_estadia
-        TO_DATE('2024-05-10', 'YYYY-MM-DD'), -- data_check_in
-        TO_DATE('2024-05-20', 'YYYY-MM-DD'), -- data_check_out
-        TP_NT_RESERVA(TP_RESERVA((SELECT REF(pt) FROM compra_tb pt WHERE pt.id_compra = 1)))
-    )
-);
-
-INSERT INTO ESTADIA VALUES (
-    ESTADIA_TP(
-        1, -- pk_cod_estadia
-        1000.00, -- valor_estadia
-        TO_DATE('2024-05-10', 'YYYY-MM-DD'), -- data_check_in
-        TO_DATE('2024-05-20', 'YYYY-MM-DD'), -- data_check_out
-        TP_NT_RESERVA(TP_RESERVA((SELECT REF(pt) FROM compra_tb pt WHERE pt.id_compra = 1)))
-    )
-);
-
-SELECT E.VALOR_ESTADIA,E.PK_COD_ESTADIA,E.DATA_CHECK_IN,E.DATA_CHECK_OUT, DEREF(DEREF(R.COMPRAS).PASSAGEIROS).NOME,
-    DEREF(DEREF(R.COMPRAS).PASSAGEIROS).PK_CPF FROM ESTADIA E, TABLE(RESERVAS) R;
 SELECT E.VALOR_ESTADIA,E.PK_COD_ESTADIA,E.DATA_CHECK_IN,E.DATA_CHECK_OUT, DEREF(DEREF(R.COMPRAS).PASSAGEIROS).NOME,
     DEREF(DEREF(R.COMPRAS).PASSAGEIROS).PK_CPF FROM ESTADIA E, TABLE(RESERVAS) R;
