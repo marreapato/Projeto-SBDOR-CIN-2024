e4760a4e8909602f02509ce5fc9f46e4ab8f307b
Lucas Rabelo <50274278+marreapato@users.noreply.github.com>
2024-04-24 21:00:36 -0300

    Update compras.sql

--- projeto_final/inserts/compras.sql
+++ projeto_final/inserts/compras.sql
@@ -3,6 +3,7 @@
 -- Inserting a purchase record for a passenger and ticket combination
 INSERT INTO COMPRA_TB VALUES (
     tp_compra(
+        1,
         TO_DATE('15/01/2024', 'DD/MM/YYYY'), -- data_compra
         (SELECT REF(p) FROM PASSAGEIRO_TB p WHERE p.PK_CPF = '12345678901'), -- passageiros REF
         (SELECT REF(pt) FROM PASSAGEM pt WHERE pt.pk_localizador_passagem = 1) -- passagem REF
@@ -12,6 +13,7 @@ INSERT INTO COMPRA_TB VALUES (
 -- Inserting another purchase record for a different passenger and ticket
 INSERT INTO COMPRA_TB VALUES (
     tp_compra(
+        2,
         TO_DATE('20/01/2024', 'DD/MM/YYYY'), -- data_compra
         (SELECT REF(p) FROM PASSAGEIRO_TB p WHERE p.PK_CPF = '98765432198'), -- passageiros REF
         (SELECT REF(pt) FROM PASSAGEM pt WHERE pt.pk_localizador_passagem = 2) -- passagem REF
@@ -21,6 +23,7 @@ INSERT INTO COMPRA_TB VALUES (
 -- Inserting another purchase record for a different passenger and ticket
 INSERT INTO COMPRA_TB VALUES (
     tp_compra(
+        3,
         TO_DATE('05/01/2024', 'DD/MM/YYYY'), -- data_compra
         (SELECT REF(p) FROM PASSAGEIRO_TB p WHERE p.PK_CPF = '11122233344'), -- passageiros REF
         (SELECT REF(pt) FROM PASSAGEM pt WHERE pt.pk_localizador_passagem = 3) -- passagem REF
