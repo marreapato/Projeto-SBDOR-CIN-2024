c077b5f39828e77f8b3a07ffaab776704634696d
Lucas Rabelo <50274278+marreapato@users.noreply.github.com>
2024-04-26 14:02:56 -0300

    Update funcions.sql

--- projeto_final/funcions.sql
+++ projeto_final/funcions.sql
@@ -2,3 +2,8 @@ CREATE OR REPLACE FUNCTION calcular_idade_trigger(data_nascimento DATE) RETURN N
 BEGIN
     RETURN TRUNC(MONTHS_BETWEEN(SYSDATE, data_nascimento) / 12);
 END calcular_idade_trigger;
+
+CREATE OR REPLACE FUNCTION calcular_idade_trigger(data_nascimento PASSAGEIRO_TB.DATA_NASCIMENTO%TYPE) RETURN NUMBER IS
+BEGIN
+    RETURN TRUNC(MONTHS_BETWEEN(SYSDATE, data_nascimento) / 12);
+END calcular_idade_trigger;
