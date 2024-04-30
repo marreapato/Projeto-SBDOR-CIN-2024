c94809b3ee591d11f68d20705ab722438553e34d
Lucas Rabelo <50274278+marreapato@users.noreply.github.com>
2024-04-30 09:33:40 -0300

    Update estadias.sql

--- TESTE_ENXUGAR_COD/INSERTS/estadias.sql
+++ TESTE_ENXUGAR_COD/INSERTS/estadias.sql
@@ -5,9 +5,9 @@ INSERT INTO ESTADIA VALUES (
     ESTADIA_TP(
         1, -- pk_cod_estadia
         1000.00, -- valor_estadia
-        TO_DATE('14/01/2025', 'DD/MM/YYYY'),
-  		TO_DATE('17/01/2025', 'DD/MM/YYYY'), -- data_check_out
-   		(SELECT REF(pt) FROM compra_tb pt WHERE pt.id_compra = 20)
+        TO_DATE('11/05/2024', 'DD/MM/YYYY'),
+  		TO_DATE('16/05/2024', 'DD/MM/YYYY'), -- data_check_out
+   		(SELECT REF(pt) FROM compra_tb pt WHERE pt.id_compra = 1)
     )
 );
 
@@ -15,8 +15,8 @@ INSERT INTO ESTADIA VALUES (
     ESTADIA_TP(
         2, -- pk_cod_estadia
         990.00, -- valor_estadia
-        TO_DATE('20/06/2024', 'DD/MM/YYYY'),
-    	TO_DATE('25/06/2024', 'DD/MM/YYYY'),
+        TO_DATE('25/09/2024', 'DD/MM/YYYY'),
+    	TO_DATE('28/09/2024', 'DD/MM/YYYY'),
         (SELECT REF(pt) FROM compra_tb pt WHERE pt.id_compra = 2))
     
 );
@@ -25,10 +25,10 @@ INSERT INTO ESTADIA VALUES (
 INSERT INTO ESTADIA VALUES (
     ESTADIA_TP(
         4, -- pk_cod_estadia
-        2990.00, -- valor_estadia
-         TO_DATE('25/09/2024', 'DD/MM/YYYY'),
-    		TO_DATE('30/09/2024', 'DD/MM/YYYY'),
-        (SELECT REF(pt) FROM compra_tb pt WHERE pt.id_compra = 5)
+        9975.32, -- valor_estadia
+         TO_DATE('14/01/2025', 'DD/MM/YYYY'),
+    		TO_DATE('23/01/2025', 'DD/MM/YYYY'),
+        (SELECT REF(pt) FROM compra_tb pt WHERE pt.id_compra = 4)
     )
 );
 
@@ -36,10 +36,10 @@ INSERT INTO ESTADIA VALUES (
 INSERT INTO ESTADIA VALUES (
     ESTADIA_TP(
         3, -- pk_cod_estadia
-        2929.30, -- valor_estadia
+        2920.30, -- valor_estadia
          TO_DATE('22/11/2024', 'DD/MM/YYYY'),
-    		TO_DATE('30/11/2024', 'DD/MM/YYYY'),
-        (SELECT REF(pt) FROM compra_tb pt WHERE pt.id_compra = 7))
+    		TO_DATE('22/11/2024', 'DD/MM/YYYY'),
+        (SELECT REF(pt) FROM compra_tb pt WHERE pt.id_compra = 3))
 );
 
 
